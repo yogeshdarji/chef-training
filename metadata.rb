ba0bf22f3304d662c2201372ab2e99ecc3e986d2
yogeshdarji <yogesh_darji99@yahoo.com>
2020-03-03 06:42:12 +0000

    version 0.2.0 release - added support for nano and emacs to setup.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '0.1.0'
+version '0.2.0'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
